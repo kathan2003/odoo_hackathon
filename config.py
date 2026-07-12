2f78d2b5de870f6dd81cb62b51b4b4584da0e134
Harsh Makvana <harsh.makvana2002@gmail.com>
2026-07-12 16:30:57 +0530

    Add files via upload

---
+++ config.py
@@ -0,0 +1,9 @@
+class Config:
+
+    SECRET_KEY = "transitops_secret_key"
+
+    # Uncomment when MySQL is ready
+
+    # SQLALCHEMY_DATABASE_URI = "mysql+pymysql://root:password@localhost/transitops"
+
+    # SQLALCHEMY_TRACK_MODIFICATIONS = False
